8a07b1125f66c47a36dd4be1799e9c14d39d44cc
Joabio <98925746+AlanJoabio@users.noreply.github.com>
2022-05-21 23:54:35 +0000

    exercicio

---
+++ Exercicio/ExercicioQuinze.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    int numero;
+
+    Scanner leitorScanner = new Scanner(System.in);
+
+    System.out.println("Digite um valor");
+
+    numero = leitorScanner.nextInt();
+
+    if(numero >= 100 && numero <= 200){
+      System.out.println("O numero esta no intervalo");
+    } else{
+      System.out.println(" o numero nao esta no intevalo");
+    }
+
+        
+
+    
+  }
+}
