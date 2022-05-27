0c6096cfff44b1096b7b5e3fe72f308814df312d
Joabio <98925746+AlanJoabio@users.noreply.github.com>
2022-05-27 22:30:55 +0000

    Ajust

---
+++ Exercicio/ExercicioDois.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Main{
+
+  public static void main (String args[]){
+
+    int x=5;
+    int y=2;
+    int z=x+y;
+    int b=x-y;
+    int a=x*y;
+    int c=x/y;
+    
+    
+
+    System.out.println("Soma: " + z);
+    System.out.println("Subtrair: " + b);
+    System.out.println("Multiplicar: " + a);
+    System.out.println("Divisão: " + c);
+  }
+}
---
+++ Exercicio/ExercicioUm.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+
+  public static void main (String args[]){
+
+    int x=5;
+    int y=5;
+    int z=x+y;
+    
+
+    System.out.println("Soma: " + z);
+  }
+}
